2cd958a8b32af0f135a2ccd422f5f9319d891e4b
ameybenare <amey.benare@gmail.com>
2023-06-19 01:25:55 +0200

    Passing dynamic data to UI

--- app.py
+++ app.py
@@ -1,85 +1,18 @@
+import Categorisation
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Sample data for job sectors and jobs
-job_sectors = [
-    {
-        'namdfe': 'IT',
-        'jobs': ['Job 1', 'Job 2', 'Job 3']
-
-    },
-    {
-        'name': 'Healthcare',
-        'jobs': ['Job 4', 'Job 5', 'Job 6']
-    },
-{
-        'name': 'Financial',
-        'jobs': ['Job 5', 'Job 6', 'Job 7']
-    },
-{
-        'name': 'Education',
-        'jobs': ['Job 7', 'Job 8', 'Job 9']
-    },
-{
-        'name': 'Financial',
-        'jobs': ['Job 5', 'Job 6', 'Job 7']
-    },
-{
-        'name': 'Education',
-        'jobs': ['Job 7', 'Job 8', 'Job 9']
-    },
-
-{
-        'name': 'Financial',
-        'jobs': ['Job 5', 'Job 6', 'Job 7']
-    },
-{
-        'name': 'Education',
-        'jobs': ['Job 7', 'Job 8', 'Job 9']
-    },
-
-{
-        'name': 'Financial',
-        'jobs': ['Job 5', 'Job 6', 'Job 7']
-    },
-{
-        'name': 'Education',
-        'jobs': ['Job 7', 'Job 8', 'Job 9']
-    },
-
-{
-        'name': 'Financial',
-        'jobs': ['Job 5', 'Job 6', 'Job 7']
-    },
-{
-        'name': 'Education',
-        'jobs': ['Job 7', 'Job 8', 'Job 9']
-    },
-
-{
-        'name': 'Financial',
-        'jobs': ['Job 5', 'Job 6', 'Job 7']
-    },
-{
-        'name': 'Education',
-        'jobs': ['Job 7', 'Job 8', 'Job 9']
-    }
-
-    # Add more sectors and their corresponding jobs here
-]
-
-
 @app.route('/')
 def index():
-    return render_template('index.html', sectors=job_sectors)
+    return render_template('index.html', sectors=Categorisation.sectorData)
 
 
 @app.route('/sector/<int:sector_id>')
 def sector(sector_id):
-    sector = job_sectors[sector_id]
+    sector = Categorisation.sectorData[sector_id]
     jobs = sector['jobs']
-    return render_template('sector.html', sector=sector, jobs=jobs)
+    return render_template('sector.html', sector=Categorisation.sectorData, jobs=jobs)
 
 
 if __name__ == '__main__':
