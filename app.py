425d51c699bc5dec42560af7a662c6624dd4c794
ParagNirwan <52133765+ParagNirwan@users.noreply.github.com>
2023-06-22 12:27:30 +0200

    Auto Images in the Sector

--- app.py
+++ app.py
@@ -5,9 +5,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     images = ["govtPublicAdmin.jpg","Healthcare.jpg","Logistics.jpg","Consulting.jpg","Hospitality.jpg","Law Enforcement.jpg","Construction.jpg",
-              "Automotive.jpg", "Manufacturing.jpg","RealEstate.jpg", "Engineering.jpg",
-              "IT.jpg","Telecommunications.jpg","Financial.jpg","Education.jpg","Food.jpg","retail.jpg","media.jpg"]
-    return render_template('index.html', sectors=Categorisation.sectorData, images=images)
+              "Automotive.jpg", "Manufacturing.jpg","Fashion.jpg","RealEstate.jpg", "Engineering.jpg",
+              "IT.jpg","Telecommunications.jpg","Financial.jpg","Education.jpg","Food.jpg","retail.jpg","media.jpg","Agriculture.jpg","Sports.png"]
+
+    images2 = {
+        "Government Public Admin": "govtPublicAdmin.jpg",
+        "Engineering": "Engineering.jpg",
+        "Manufacturing": "Manufacturing.jpg",
+        "Retail": "retail.jpg",
+        "Real Estate": "RealEstate.jpg",
+        "Law Enforcement": "Law Enforcement.jpg",
+        "Information Technology": "IT.jpg",
+        "Hospitality Tourism": "Hospitality.jpg",
+        "Healthcare": "Healthcare.jpg",
+        "Financial": "Financial.jpg",
+        "Telecommunication": "Telecommunications.jpg",
+        "Automotive": "Automotive.jpg",
+        "Transportation Logistics": "Logistics.jpg",
+        "Media": "media.jpg",
+        "Education": "Education.jpg",
+        "Fashion": "Fashion.jpg",
+        "Food and Beverages": "Food.jpg",
+        "Construction": "Construction.jpg",
+        "Sports and Fitness": "Sports.png",
+        "Consulting": "Consulting.jpg"
+    }
+
+    return render_template('index.html', sectors=Categorisation.sectorData, images=images2)
 
 
 @app.route('/sector/<int:sector_id>')
